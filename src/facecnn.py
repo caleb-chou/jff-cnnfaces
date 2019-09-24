3fa58c1b1f8abe3fe85afb4707cc3a86e4927ff3
caleb <haloisawesome412@gmail.com>
2019-09-24 16:48:34 -0500

    changed to use mtcnn

--- src/facecnn.py
+++ src/facecnn.py
@@ -1,26 +1,19 @@
 import cv2
-import sys
-
-cascPath = "../resources/models/haarcascade_frontalface_default.xml"
-faceCasc = cv2.CascadeClassifier(cascPath)
+from mtcnn.mtcnn import MTCNN
 
 video = cv2.VideoCapture(0)
+d = MTCNN()
 
 while True:
     r, f = video.read()
-    
-    g = cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
-    faces = faceCasc.detectMultiScale(
-        g, 
-        scaleFactor = 1.1,
-        minNeighbors = 5,
-        minSize = (28,28),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
-    
-    for (x,y,w,h) in faces:
+    g = cv2.cvtColor(f,cv2.COLOR_BGR2RGB)
+    faces = d.detect_faces(g)
+    for face in faces:
+        x = face.get('box')[0]
+        y = face.get('box')[1]
+        w = face.get('box')[2]
+        h = face.get('box')[3]
         cv2.rectangle(f,(x,y), (x+w,y+h), (0,255,0), 2)
-    
     cv2.imshow('Video',f)
     if (cv2.waitKey(1) & 0xFF == ord('q')):
         break;
